e794fdf447c8d009423f557a0e4d8415fc2d8d1c
Juan Sebastián Ramírez Ayala <Sebastian.Ramirez@blend360.com>
2026-06-01 12:05:21 -0500

    feat(cdk): add hello world lambda stack

--- bin/s2026-q2_c-cdk-sebastian-ramirez.ts
+++ bin/s2026-q2_c-cdk-sebastian-ramirez.ts
@@ -1,20 +1,19 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib/core';
-import { S2026Q2CCdkSebastianRamirezStack } from '../lib/s2026-q2_c-cdk-sebastian-ramirez-stack';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { S2026Q2CCdkSebastianRamirezStack } from "../lib/s2026-q2_c-cdk-sebastian-ramirez-stack";
 
 const app = new cdk.App();
-new S2026Q2CCdkSebastianRamirezStack(app, 'S2026Q2CCdkSebastianRamirezStack', {
+new S2026Q2CCdkSebastianRamirezStack(app, "S2026Q2CCdkSebastianRamirezStack", {
+  env: { account: "891377180652", region: "us-east-1" },
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
-
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
   // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
   // env: { account: '123456789012', region: 'us-east-1' },
-
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
--- lib/s2026-q2_c-cdk-sebastian-ramirez-stack.ts
+++ lib/s2026-q2_c-cdk-sebastian-ramirez-stack.ts
@@ -1,10 +1,25 @@
-import * as cdk from 'aws-cdk-lib/core';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class S2026Q2CCdkSebastianRamirezStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    // Define the Lambda function
+    const myFunction = new lambda.Function(this, "HellowWorld", {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: "index.handler",
+      code: lambda.Code.fromInline(`
+        exports.handler = async (event) => {
+          return {
+            statusCode: 200,
+            body: JSON.stringify('Hello, World!'),
+          };
+        };
+      `),
+    });
 
     // The code that defines your stack goes here
 
