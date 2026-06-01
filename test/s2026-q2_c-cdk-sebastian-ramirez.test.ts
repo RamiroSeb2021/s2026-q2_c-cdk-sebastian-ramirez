0c8c0162159ff73692bd4cf752a10e819934eeba
Juan Sebastián Ramírez Ayala <Sebastian.Ramirez@blend360.com>
2026-06-01 10:28:44 -0500

    Initial commit

---
+++ test/s2026-q2_c-cdk-sebastian-ramirez.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as S2026Q2CCdkSebastianRamirez from '../lib/s2026-q2_c-cdk-sebastian-ramirez-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/s2026-q2_c-cdk-sebastian-ramirez-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new S2026Q2CCdkSebastianRamirez.S2026Q2CCdkSebastianRamirezStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
